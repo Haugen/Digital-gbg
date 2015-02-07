09dcc09699e4eca0fdfb793852820624a7c414f6
Haugen <tobiashaugen@gmail.com>
2015-02-07 17:32:34 +0100

    Changed some names

--- app/templates/header.tpl.php
+++ app/templates/header.tpl.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="Twitterino">
+  <meta name="description" content="Digital gbg">
   <meta name="author" content="Tobias Haugen">
 
   <title>Digital gbg</title>
