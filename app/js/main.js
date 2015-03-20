8ef5a7da5b2aedd92a794b7d96f5557175623b17
Tobias Haugen <tobiashaugen@gmail.com>
2015-03-20 15:31:54 +0100

    More removal

--- app/js/main.js
+++ app/js/main.js
@@ -1,111 +1,15 @@
 var studios = {
-  'bullet' : {
-    'title'   : 'Bullet',
-    'website' : 'http://bullet.se/',
-    'lat'     : 57.7024408,
-    'lng'     : 11.9919017
-  },
-  'callista' : {
-    'title'   : 'Callista',
-    'website' : 'http://callistaenterprise.se/',
-    'lat'     : 57.7048675,
-    'lng'     : 11.992149
-  },
-  'cordovan' : {
-    'title'   : 'Cordovan',
-    'website' : 'http://www.cordovan.se/',
-    'lat'     : 57.703723,
-    'lng'     : 11.9678293
-  },
   'cpb' : {
     'title'   : 'CP+B',
     'website' : 'http://www.cpbgroup.com/',
     'lat'     : 57.7003216,
     'lng'     : 11.9552149
   },
-  'creuna' : {
-    'title'   : 'Creuna',
-    'website' : 'http://www.creuna.se/',
-    'lat'     : 57.7042,
-    'lng'     : 11.9709
-  },
   'dear-friends' : {
     'title'   : 'Dear Friends',
     'website' : 'http://dearfriends.se/',
     'lat'     : 57.7086734,
     'lng'     : 11.9631469
-  },
-  'dfind-it' : {
-    'title'   : 'Dfind IT',
-    'website' : 'https://www.dfind.se/it/',
-    'lat'     : 57.7063456,
-    'lng'     : 11.9679436
-  },
-  'digitaslbi' : {
-    'title'   : 'DigitasLBi',
-    'website' : 'http://www.digitaslbi.com/se',
-    'lat'     : 57.7051801,
-    'lng'     : 11.9713869
-  },
-  'direktonline' : {
-    'title'   : 'Direktonline',
-    'website' : 'http://direktonline.se/',
-    'lat'     : 57.699012,
-    'lng'     : 11.9679508
-  },
-  'doppio-digital' : {
-    'title'   : 'Doppio digital',
-    'website' : 'http://doppio.se/',
-    'lat'     : 57.7038361,
-    'lng'     : 11.955436
-  },
-  'dragster' : {
-    'title'   : 'Dragster',
-    'website' : 'http://dragster.se/',
-    'lat'     : 57.7005379,
-    'lng'     : 11.984862
-  },
-  'endeavor' : {
-    'title'   : 'Endeavor',
-    'website' : 'http://www.endeavor.se/',
-    'lat'     : 57.7038744,
-    'lng'     : 11.9575395
-  },
-  'exsitec' : {
-    'title'   : 'Exsitec',
-    'website' : 'http://www.exsitec.se/',
-    'lat'     : 57.7041525,
-    'lng'     : 11.971367
-  },
-  'ework' : {
-    'title'   : 'Ework',
-    'website' : 'https://www.ework.se/',
-    'lat'     : 57.7041809,
-    'lng'     : 11.9699931
-  },
-  'ework' : {
-    'title'   : 'Ework',
-    'website' : 'https://www.ework.se/',
-    'lat'     : 57.7041809,
-    'lng'     : 11.9699931
-  },
-  'factory-fb' : {
-    'title'   : 'Factory, Forsman & Bodenfors',
-    'website' : 'http://www.factory.fb.se/',
-    'lat'     : 57.7047545,
-    'lng'     : 11.969374
-  },
-  'findwise' : {
-    'title'   : 'Findwise',
-    'website' : 'http://www.findwise.com/',
-    'lat'     : 57.7067147,
-    'lng'     : 11.9837918
-  },
-  'hiq' : {
-    'title'   : 'HiQ',
-    'website' : 'http://www.hiq.se/',
-    'lat'     : 57.7072301,
-    'lng'     : 11.9691746
   },
   'humblebee' : {
     'title'   : 'Humblebee',
@@ -119,12 +23,6 @@ var studios = {
     'lat'     : 57.703735,
     'lng'     : 11.962217
   },
-  'inuse' : {
-    'title'   : 'Inuse',
-    'website' : 'http://www.inuse.se/',
-    'lat'     : 57.7039047,
-    'lng'     : 11.9693537
-  },
   'itiden' : {
     'title'   : 'Itiden',
     'website' : 'http://www.itiden.se/',
@@ -137,53 +35,17 @@ var studios = {
     'lat'     : 57.6997764,
     'lng'     : 11.9487528
   },
-  'knowit' : {
-    'title'   : 'Knowit',
-    'website' : 'http://www.knowit.se/',
-    'lat'     : 57.7094591,
-    'lng'     : 11.9668683
+  'lineducation' : {
+    'title'   : 'Lin Education',
+    'website' : 'http://lineducation.se/',
+    'lat'     : 57.708201,
+    'lng'     : 11.944475
   },
-  'layer-10' : {
-    'title'   : 'Layer 10',
-    'website' : 'http://layer10.se/',
-    'lat'     : 57.6811984,
-    'lng'     : 12.0007895
-  },
-  'linderoths' : {
-    'title'   : 'Linderoths',
-    'website' : 'https://linderoths.se/',
-    'lat'     : 57.7032268,
-    'lng'     : 11.9655714
-  },
-  'lundgren-lindqvist' : {
-    'title'   : 'Lundgren+Lindqvist',
-    'website' : 'http://www.lundgrenlindqvist.se/',
-    'lat'     : 57.6891231,
-    'lng'     : 11.9060197
-  },
-  'made-people' : {
-    'title'   : 'Made People',
-    'website' : 'http://madepeople.se/',
-    'lat'     : 57.7029277,
-    'lng'     : 11.9568957
-  },
-  'mild-media' : {
-    'title'   : 'Mild Media',
-    'website' : 'http://www.mildmedia.se/',
-    'lat'     : 57.6995669,
-    'lng'     : 11.9368491
-  },
-  'mobiento' : {
-    'title'   : 'Mobiento',
-    'website' : 'http://www.mobiento.com/',
-    'lat'     : 57.7055417,
-    'lng'     : 11.966617
-  },
-  'mogul' : {
-    'title'   : 'Mogul',
-    'website' : 'http://www.mogul.com/',
-    'lat'     : 57.7010836,
-    'lng'     : 11.9759599
+  'lexicon' : {
+    'title'   : 'Lexicon',
+    'website' : 'http://www.lexicon.se/',
+    'lat'     : 57.705857,
+    'lng'     : 11.964815
   },
   'monocl' : {
     'title'   : 'Monocl',
@@ -191,35 +53,11 @@ var studios = {
     'lat'     : 57.7063865,
     'lng'     : 11.9614117
   },
-  'nethouse' : {
-    'title'   : 'Nethouse',
-    'website' : 'http://www.nethouse.se/',
-    'lat'     : 57.7059942,
-    'lng'     : 11.9919264
-  },
   'netrelations' : {
     'title'   : 'NetRelations',
     'website' : 'https://www.netrelations.com/sv/',
     'lat'     : 57.7061879,
     'lng'     : 11.9686219
-  },
-  'ninetech' : {
-    'title'   : 'Ninetech',
-    'website' : 'http://www.ninetech.com/sv/',
-    'lat'     : 57.7040849,
-    'lng'     : 11.9571236
-  },
-  'nute' : {
-    'title'   : 'Nute',
-    'website' : 'http://www.nute.se/',
-    'lat'     : 57.7029201,
-    'lng'     : 11.9617392
-  },
-  'omega-point' : {
-    'title'   : 'Omega Point',
-    'website' : 'http://www.omegapoint.se/',
-    'lat'     : 57.7009613,
-    'lng'     : 11.9590663
   },
   'ottoboni' : {
     'title'   : 'Ottoboni',
@@ -227,47 +65,11 @@ var studios = {
     'lat'     : 57.713406,
     'lng'     : 11.9676541
   },
-  'prevas' : {
-    'title'   : 'Prevas',
-    'website' : 'http://www.prevas.se/',
-    'lat'     : 57.649245,
-    'lng'     : 11.9263364
-  },
-  'provideit' : {
-    'title'   : 'Provide it',
-    'website' : 'http://provideit.se/',
-    'lat'     : 57.7032596,
-    'lng'     : 11.9589511
-  },
-  'pwr-communication' : {
-    'title'   : 'PWR Communication',
-    'website' : 'http://www.pwr.se/',
-    'lat'     : 57.7052957,
-    'lng'     : 11.9659043
-  },
-  'quality-minds' : {
-    'title'   : 'Quality Minds',
-    'website' : 'http://www.qualityminds.se/',
-    'lat'     : 57.7152657,
-    'lng'     : 11.903382
-  },
-  'rabash' : {
-    'title'   : 'Rabash',
-    'website' : 'http://rabash.se/',
-    'lat'     : 57.7003845,
-    'lng'     : 11.954508
-  },
   'raket' : {
     'title'   : 'Raket',
     'website' : 'http://www.raketwebbyra.se/',
     'lat'     : 57.699575,
     'lng'     : 11.9650444
-  },
-  'sandberg-trygg' : {
-    'title'   : 'Sandberg Trygg',
-    'website' : 'http://www.sandbergtrygg.se/',
-    'lat'     : 57.7041809,
-    'lng'     : 11.9699931
   },
   'schimpanz' : {
     'title'   : 'Schimpanz',
@@ -281,72 +83,6 @@ var studios = {
     'lat'     : 57.7127473,
     'lng'     : 11.9479666
   },
-  'sigma-itm' : {
-    'title'   : 'Sigma IT&M',
-    'website' : 'http://sigmaitm.se/',
-    'lat'     : 57.7062402,
-    'lng'     : 11.9405419
-  },
-  'silverbullet-film' : {
-    'title'   : 'Silverbullet Film',
-    'website' : 'http://www.silverbulletfilm.com/',
-    'lat'     : 57.6873126,
-    'lng'     : 11.9987026
-  },
-  'softhouse' : {
-    'title'   : 'Softhouse',
-    'website' : 'http://www.softhouse.se/',
-    'lat'     : 57.7037626,
-    'lng'     : 11.9624833
-  },
-  'solidbeans' : {
-    'title'   : 'SolidBeans',
-    'website' : 'http://www.solidbeans.se/',
-    'lat'     : 57.7038226,
-    'lng'     : 11.9621599
-  },
-  'sogeti' : {
-    'title'   : 'Sogeti',
-    'website' : 'http://www.sogeti.se/',
-    'lat'     : 57.7065026,
-    'lng'     : 11.9699953
-  },
-  'spiro' : {
-    'title'   : 'Spiro',
-    'website' : 'http://spiro.se/',
-    'lat'     : 57.7037982,
-    'lng'     : 11.9661591
-  },
-  'squeed' : {
-    'title'   : 'Squeed',
-    'website' : 'http://www.squeed.com/',
-    'lat'     : 57.70263,
-    'lng'     : 11.9634389
-  },
-  'star-republic' : {
-    'title'   : 'Star Republic',
-    'website' : 'http://www.starrepublic.com/',
-    'lat'     : 57.7035883,
-    'lng'     : 11.9608677
-  },
-  'stendahls' : {
-    'title'   : 'Stendahls',
-    'website' : 'http://www.stendahls.se/',
-    'lat'     : 57.6976337,
-    'lng'     : 11.9643261
-  },
-  'studio-interact' : {
-    'title'   : 'Studio Interact',
-    'website' : 'http://studiointeract.se/',
-    'lat'     : 57.7067047,
-    'lng'     : 11.9837818
-  },
-  'supernormal' : {
-    'title'   : 'Supernormal',
-    'website' : 'http://supernormal.se/',
-    'lat'     : 57.7030576,
-    'lng'     : 11.9649688
-  },
   'understandit' : {
     'title'   : 'UnderstandIt',
     'website' : 'https://understandit.se/',
@@ -359,60 +95,6 @@ var studios = {
     'lat'     : 57.7040412,
     'lng'     : 11.9671915
   },
-  'wallmander-co' : {
-    'title'   : 'Wallmander & co',
-    'website' : 'http://www.wallmanderco.se/',
-    'lat'     : 57.706644,
-    'lng'     : 11.9617434
-  },
-  'we-made-you-look' : {
-    'title'   : 'We Made You Look',
-    'website' : 'http://www.wmyl.se/',
-    'lat'     : 57.699688,
-    'lng'     : 11.966011
-  },
-  'webbhuset' : {
-    'title'   : 'Webbhuset',
-    'website' : 'http://www.webbhuset.se/',
-    'lat'     : 57.7047095,
-    'lng'     : 11.9677988
-  },
-  'webmind' : {
-    'title'   : 'Webmind',
-    'website' : 'http://www.webmind.se/',
-    'lat'     : 57.7000858,
-    'lng'     : 11.970721
-  },
-  'weightpoint' : {
-    'title'   : 'Weightpoint',
-    'website' : 'http://www.weightpoint.se/',
-    'lat'     : 57.7037726,
-    'lng'     : 11.9624933
-  },
-  'Welcome' : {
-    'title'   : 'Welcome',
-    'website' : 'http://www.welcom.se/',
-    'lat'     : 57.7074652,
-    'lng'     : 11.9696669
-  },
-  'wipcore' : {
-    'title'   : 'Wipcore',
-    'website' : 'http://www.wipcore.se/',
-    'lat'     : 57.7142578,
-    'lng'     : 11.9728844
-  },
-  'xelera' : {
-    'title'   : 'Xelera',
-    'website' : 'http://www.xelera.se/',
-    'lat'     : 57.704975,
-    'lng'     : 11.969246
-  },
-  'zooma' : {
-    'title'   : 'Zooma',
-    'website' : 'http://www.zooma.se/',
-    'lat'     : 57.7063754,
-    'lng'     : 11.9444496
-  }
 };
 
 $(document).ready(function() {
